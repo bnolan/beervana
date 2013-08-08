4e4598e4a0693dcde8b9fcb2386720812f6cd5f7
Ben Nolan <ben@nolanconsul.com>
2013-08-08 15:58:07 +1200

    Got rough authentcation working.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  
+  def create
+    user = User.new(user_params)
+    
+    if user.save
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :text => "Error - user name " + user.errors[:name].to_sentence
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :name
+  validates_presence_of :name
 end
