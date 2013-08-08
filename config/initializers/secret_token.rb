78f9aaae88cbba60dc25ce190a539c069e3e3d10
Ben Nolan <ben@nolanconsul.com>
2013-08-08 15:10:40 +1200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beervana::Application.config.secret_key_base = '76e0e6f30d661c9f3c3b36328d7eb7cf1f4ca62a77ed84e10fc3ae23fc30bb3c1359a056854d2270d345ffe158698f6b2404765ba3a7f53a0319c8b456d2ce6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Beervana::Application.config.session_store :cookie_store, key: '_beervana_session'
