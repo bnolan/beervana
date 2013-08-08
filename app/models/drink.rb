33a9f17c6c2a04af24c549bbd5cdc4cefd3f7120
Andy Newport <andy@youdo.co.nz>
2013-08-08 18:48:00 +1200

    Populate session

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -2,22 +2,40 @@ class Drink < ActiveRecord::Base
   belongs_to :beer, :counter_cache => true
   belongs_to :user
   validates_presence_of :volume, :beer_id, :user_id, :rating
-  after_save :update_beer_average_rating
+  validate :session_is_set
   validates_inclusion_of :rating, :within => 1..5, :message => "must be between 1 and 5"
-  
-  def before_save
-    self.session = 1 # todo - implement session numbering
-  end
-  
+
+  before_validation :populate_session, :on => :create
+  after_save :update_beer_average_rating
+
   def alcohol_in_mls
     # @proglottis pointed out that this isn't correct, but it's an approximation
     volume * (beer.abv / 100.0)
   end
 
-private
+  SESSIONS = {
+    1 => Time.new(2013, 8, 9,  11)..Time.new(2013, 8, 9,  16),
+    2 => Time.new(2013, 8, 9,  18)..Time.new(2013, 8, 9,  23),
+    3 => Time.new(2013, 8, 10, 11)..Time.new(2013, 8, 10, 16),
+    4 => Time.new(2013, 8, 10, 18)..Time.new(2013, 8, 10, 23)
+  }
 
+private
   def update_beer_average_rating
     beer.update_average_rating
   end
-  
+
+  def current_session
+    match = SESSIONS.find { |(session,time_range)| time_range.cover?(Time.now) }
+    match[0] if match
+  end
+
+  def populate_session
+    self.session = current_session
+  end
+
+  # Validation
+  def session_is_set
+     self.errors[:session] << "CHEATER!!! You can't log drinks outside session times." if session.blank?
+  end
 end
