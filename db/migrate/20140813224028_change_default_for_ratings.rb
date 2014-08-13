fbde01df3b3f81bf7129336026f2b40f1f6bfdb0
Ben Nolan <ben@nolanconsul.com>
2014-08-14 10:46:29 +1200

    Oops.

--- db/migrate/20140813224028_change_default_for_ratings.rb
+++ db/migrate/20140813224028_change_default_for_ratings.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultForRatings < ActiveRecord::Migration
   def change
-    change_column :beer_ratings, :average_rating, :default => 3.0, :null => false
+    change_column :beer_ratings, :average_rating, :float, :default => 3.0, :null => false
   end
 end
