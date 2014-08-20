8b9a7d5a61c43d5ba29e37c36f857a4aff6bb789
Ben Nolan <ben@nolanconsul.com>
2014-08-20 17:33:32 +1200

    Prevent login/pass from being logged due to proguard over-optimizing the post requests.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Beervana
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # There was a bug in the proguard build of the android app that submitted l/p as a: and :b and :c
+    config.filter_parameters += [:a, :b, :c]
     
     config.autoload_paths += %W(#{config.root}/lib)
   end
