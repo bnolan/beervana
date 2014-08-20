e80dbff40a02a57033f17419d6d20b4250768853
Ben Nolan <ben@nolanconsul.com>
2014-08-20 17:07:10 +1200

    Disable my ensure beer ratings method, didn't see that roger had already fixed that bug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  before_filter :ensure_beer_ratings_generated
+  # before_filter :ensure_beer_ratings_generated
   
   def requires_current_user
     if current_user.present?
